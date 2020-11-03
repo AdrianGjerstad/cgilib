c8b537e521c1e50fc712a22f4cecfaa04301d57e
AdrianGjerstad <48812634+AdrianGjerstad@users.noreply.github.com>
2020-11-03 17:57:42 -0500

    Set up Travis CI

---
+++ test/basic.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "cgilib.h"
+
+int main(int argc, char** argv) {
+  cgi::headers.add("Content-Type", "text/plain");
+
+  cgi::out << "It works!" << std::endl;
+
+  cgi::send_response();
+  
+  return 0;
+}
+
