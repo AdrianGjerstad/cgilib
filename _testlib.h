43128d507b25dd7ee20fc4120cd6c7cb6bfeb7cf
AdrianGjerstad <48812634+AdrianGjerstad@users.noreply.github.com>
2020-11-04 13:40:57 -0500

    Begin testing library

---
+++ _testlib.h
@@ -0,0 +1,13 @@
+#ifndef CGILIB_TESTING_LIBRARY_H_
+#define CGILIB_TESTING_LIBRARY_H_
+
+namespace cgi {
+namespace test {
+
+
+
+} // namespace test
+} // namespace cgi
+
+#endif  // CGILIB_TESTING_LIBRARY_H_
+
